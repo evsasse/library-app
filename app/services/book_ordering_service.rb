4e3c971a73810f1a380def07fe6b834497d63ccf
Miratan Lehmkuhl <miratansc@gmail.com>
2018-04-10 22:52:02 -0300

    logic to dynamically order each attribute by its direction

--- app/services/book_ordering_service.rb
+++ app/services/book_ordering_service.rb
@@ -12,6 +12,27 @@ class BookOrderingService
   def do_ordering
     return {} if @books.empty?
 
+    @books = @books.sort_by do |book|
+      next unless @ordering_file.present? && @ordering_file['book'].present?
+      b = []
+
+      @ordering_file['book'].each do |key, value|
+        attribute = book[key.to_sym]
+
+        field = if !attribute.to_i.zero? && attribute != '0' && value == 'asc'
+                  attribute.to_i
+                elsif !attribute.to_i.zero? && attribute != '0'
+                  attribute.to_i * -1
+                elsif value == 'asc'
+                  attribute
+                else
+                  attribute.reverse
+                end
+
+        b << field
+      end
+      b
+    end
 
     @books
   end
